c49f541e9d68306d985ff039c269888000d1344d
Adhithyaa Kumar S <147901324+Adhithya2403@users.noreply.github.com>
2026-08-24 09:53:49 +0530

    Time: 2 ms (44.57%), Space: 46 MB (93.13%) - LeetHub

--- 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.java
+++ 2828-check-if-a-string-is-an-acronym-of-words/2828-check-if-a-string-is-an-acronym-of-words.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean isAcronym(List<String> words, String s) {
         String b="";
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<words.size();i++)
         {
             String ab =words.get(i);
-            b+=ab.charAt(0);
+            sb.append(ab.charAt(0));
         }
-        return b.equals(s);
+        return sb.toString().equals(s);
     }
 }
